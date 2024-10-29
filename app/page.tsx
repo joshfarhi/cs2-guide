aa5ff09dc6f37db1db126284f985f29c5f3e2f5b
Joshua <joshuafarhi@protonmail.com>
2024-10-28 18:47:41 -0700

    added new inferno badge

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,21 @@ export default function Home() {
       <h4 className="text-xl font-bold mb-8 text-foreground">Choose a map below.</h4>
       <div className="space-y-4">
         <Link href="/mirage" legacyBehavior>
-          <a className="block w-64 py-4 text-center text-xl font-semibold text-foreground bg-transparent rounded-lg shadow-lg transform transition-transform hover:scale-105 items-center justify-center">
-            <Image src="/images/mirage-badge.png" alt="Mirage Badge" width={24} height={24} className="w-12 h-12 mb-2 mx-auto" />
-            Mirage
+          <a className="block w-64 py-4 text-center text-xl font-semibold text-foreground bg-transparent rounded-lg transform transition-transform hover:scale-105 items-center justify-center">
+            <Image src="/images/mirage-badge.png" alt="Mirage Badge" width={128} height={128} className="w-32 h-32 mb-2 mx-auto" />
+            
           </a>
         </Link>
         <Link href="/dust2" legacyBehavior>
-          <a className="block w-64 py-4 text-center text-xl font-semibold text-white bg-green-500 rounded-lg hover:bg-green-600">
-            Dust 2
+          <a className="block w-64 py-4 text-center text-xl font-semibold text-foreground bg-transparent rounded-lg transform transition-transform hover:scale-105 items-center justify-center">
+            <Image src="/images/dust2-badge.png" alt="Dust2 Badge" width={128} height={128} className="w-32 h-32 mb-2 mx-auto" />
+            
           </a>
         </Link>
         <Link href="/inferno" legacyBehavior>
-          <a className="block w-64 py-4 text-center text-xl font-semibold text-white bg-red-500 rounded-lg hover:bg-red-600">
-            Inferno
+          <a className="block w-64 py-4 text-center text-xl font-semibold text-foreground bg-transparent rounded-lg transform transition-transform hover:scale-105 items-center justify-center">
+            <Image src="/images/inferno-badge.png" alt="Dust2 Badge" width={128} height={128} className="w-32 h-32 mb-2 mx-auto" />
+            
           </a>
         </Link>
         <Link href="/vertigo" legacyBehavior>
